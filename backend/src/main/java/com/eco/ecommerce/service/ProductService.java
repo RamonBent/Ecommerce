5d5e5d545c236145f0968bff91a027052b358b7e
ramon <ramon.bento.oliver@gmail.com>
2025-08-03 19:57:24 -0300

    Criando product service

---
+++ backend/src/main/java/com/eco/ecommerce/service/ProductService.java
@@ -0,0 +1,37 @@
+package com.eco.ecommerce.service;
+
+import com.eco.ecommerce.dto.mapper.ProductMapper;
+import com.eco.ecommerce.dto.response.ProductResponse;
+import com.eco.ecommerce.dto.resquest.ProductRequest;
+import com.eco.ecommerce.model.Product;
+import com.eco.ecommerce.repository.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ProductService {
+    @Autowired
+    private ProductRepository productRepo;
+    @Autowired
+    private ProductMapper mapper;
+
+    public List<ProductResponse> listAll() {
+        return productRepo.findAll().stream().map(mapper::toResponse).toList();
+    }
+
+    public ProductResponse save(ProductRequest req) {
+        Product p = mapper.toEntity(req);
+        productRepo.save(p);
+        return mapper.toResponse(p);
+    }
+
+    public ProductResponse findById(Long id) {
+        return mapper.toResponse(productRepo.findById(id).orElseThrow());
+    }
+
+    public void delete(Long id) {
+        productRepo.deleteById(id);
+    }
+}
